1ebaad0d48df737c39763cf0e398b7f648d08759
Bonnie <luk23bonnie8@gmail.com>
2023-07-27 22:04:04 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:smart_health/screens/doctor_screen.dart';
+import 'package:smart_health/screens/login.dart';
+import 'package:smart_health/screens/patient_screen.dart';
+import 'package:smart_health/screens/pharmacist_screen.dart';
+import 'package:smart_health/screens/signup.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,6 +22,14 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Smart Health',
       theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
+      initialRoute: '/register',
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/register': (context) => RegistrationScreen(),
+        '/patient-home': (context) => const PatientHomeScreen(),
+        '/doctor-home': (context) => const DoctorHomeScreen(),
+        '/pharmacist-home': (context) => const PharmacistHomeScreen(),
+      },
     );
   }
 }
