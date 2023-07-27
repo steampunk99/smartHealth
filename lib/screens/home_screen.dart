d6a21489d0bc27dd39742a14da27aef463f167d1
Bonnie <luk23bonnie8@gmail.com>
2023-07-27 22:04:45 +0300

    .

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:smart_health/services/auth_service.dart';
+import 'package:smart_health/models/user_role.dart';
+
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
+
+  final AuthService _authService = AuthService();
+
+  void _getUserRole() async {
+    UserRole? userRole = await _authService.getUserRole();
+    if (userRole == UserRole.patient) {
+    } else if (userRole == UserRole.pharmacist) {
+    } else if (userRole == UserRole.doctor) {
+    } else {
+      //do nothing yet
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _getUserRole();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dynaxo Health"),
+      ),
+      body: Center(child: Text('Loading User Roles')),
+    );
+  }
+}
